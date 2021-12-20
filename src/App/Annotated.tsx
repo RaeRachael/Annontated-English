94486af18fe5a5be92c611881989cd132bb97a7c
Rachael Ewins <rachaelewins@hotmail.com>
2021-12-20 12:56:24 +0000

    san-serif as font

--- src/App/Annotated.tsx
+++ src/App/Annotated.tsx
@@ -3,7 +3,9 @@ import React from "react";
 function Annotated(props: { text: string }) {
   return (
     <div className="Annotated">
-      <div style={{ fontSize: "30px" }}>{props.text}</div>
+      <div style={{ fontSize: "30px", fontFamily: "sans-serif" }}>
+        {props.text}
+      </div>
     </div>
   );
 }
